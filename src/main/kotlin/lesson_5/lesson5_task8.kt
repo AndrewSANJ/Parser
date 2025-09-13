015a917fee3d918b3ee0b47bd720819788257025
Andrey S <hawaiisan@yandex.ru>
2025-09-13 13:42:16 +0300

    parsing

--- src/main/kotlin/lesson_5/lesson5_task8.kt
+++ src/main/kotlin/lesson_5/lesson5_task8.kt
@@ -1,5 +1,26 @@
 package org.example.lesson_5
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
 fun main() {
+
+    val doc: Document = Jsoup.connect(
+        "https://mybook.ru/author/duglas-adams/" +
+                "avtostopom-po-galaktike-restoran-u-konca-vselennoj/" +
+                "citations/"
+    ).get()
+
+    println("Цитаты с первой страницы: ")
+
+    val quotes = doc.select(".sc-2aegk7-2")
+
+    var number = 1
+
+    for (quote in quotes) {
+
+        println("${number++} - ${quote.text()}")
+    }
+
 
 }
